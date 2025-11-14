04607d5777387f8dc9a2a437242c3acdf113d1d0
shehara Accura <sheharaaccura@gmail.com>
2025-11-14 16:13:55 +0530

    Initial commit

---
+++ src/main/java/com/hrmanager/hrmatrix/HrmatrixApplication.java
@@ -0,0 +1,13 @@
+package com.hrmanager.hrmatrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmatrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrmatrixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hrmanager/hrmatrix/HrmatrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrmanager.hrmatrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmatrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
